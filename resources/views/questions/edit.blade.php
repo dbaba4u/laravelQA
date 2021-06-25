f64b0114973e461932e1b2c4db1c22721dc362ce
Dauda Baba <deebaba4u@gmail.com>
2021-06-25 16:59:29 +0100

    Updating Question

--- resources/views/questions/edit.blade.php
+++ resources/views/questions/edit.blade.php
@@ -6,7 +6,7 @@
             <div class="card">
                 <div class="card-header">
                     <div class="d-flex align-items-center">
-                        <h2>{{ __('Ask Question') }}</h2>
+                        <h2>{{ __('Edit Question') }}</h2>
                         <div class="ml-auto">
                             <a href="{{route('questions.index')}}" class="btn btn-outline-success">Back to all question</a>
                         </div>
@@ -14,8 +14,9 @@
                 </div>
 
                 <div class="card-body">
-                    <form action="{{route('questions.store')}}" method="post">
-                       @include('questions._form',['buttonText'=>'Ask Question'])
+                    <form action="{{route('questions.update',$question->id)}}" method="post">
+                        @method('PUT')
+                       @include('questions._form',['buttonText'=>'Update Question'])
                     </form>
                 </div>
             </div>
